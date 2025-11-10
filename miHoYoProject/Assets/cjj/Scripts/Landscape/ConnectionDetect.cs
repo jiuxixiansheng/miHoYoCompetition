e8cfbe392a8ccbc9ca32d8fda5541be016e0f6b2
CodeDay <uniqueX2024@outlook.com>
2025-11-10 19:47:27 +0800

    add final landscape detection

--- miHoYoProject/Assets/cjj/Scripts/Landscape/ConnectionDetect.cs
+++ miHoYoProject/Assets/cjj/Scripts/Landscape/ConnectionDetect.cs
@@ -20,7 +20,8 @@ public class ConnectionDetect : MonoBehaviour
         LandscapeData landscapeDataCur = gameObject.GetComponent<LandscapeData>();
         LandscapeData landscapeDataNxt = other.gameObject.GetComponent<LandscapeData>();
         landscapeDataCur.correctlyConnected += (
-            (landscapeDataCur.orderInRythm + 1) % 5 == landscapeDataNxt.orderInRythm) ? 1 : 0;
+            ((landscapeDataCur.orderInRythm + 1) % 5 == landscapeDataNxt.orderInRythm)) || 
+            (landscapeDataNxt.tag == "Final") ? 1 : 0;
         Debug.Log("Collision with:" + other.gameObject.name);
         if (landscapeDataCur.correctlyConnected > 0)
         {
